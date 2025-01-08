33b115f7f9e3ae1f1da30c3c7511f04a05b09a58
dong4j <dong4j@gmail.com>
2025-01-08 16:03:01 +0800

    更新页面

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 获取当前脚本的所在目录
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+# 切换到 Makefile 所在的工作目录 (即脚本所在目录的父目录)
+cd "$SCRIPT_DIR" || exit 1
+
+# 定义本地目录
+LOCAL_DIR="public" # 脚本同级目录下的 public
+TAG_DIR="$SCRIPT_DIR/../source/wechatoa"
+
+# 上传文件到远程并覆盖
+echo "正在拷贝 public 目录下的文件到 wechatoa"
+rsync -azqhP --delete \
+  --exclude '.DS_Store' \
+  --exclude '._*' \
+  --exclude '__MACOSX' \
+  "$LOCAL_DIR/" "$TAG_DIR" | tee /dev/null
+
+# 检查上传是否成功
+if [ $? -eq 0 ]; then
+  echo "文件拷贝成功！"
+else
+  echo "文件拷贝失败。"
+  exit 1
+fi
---
+++ git-commit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 获取当前脚本的所在目录
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+cd "$SCRIPT_DIR" || exit 1
+
+# 使用第一个参数作为提交信息，如果未提供参数，则使用默认信息
+COMMIT_MESSAGE=${1:-"更新页面"}
+
+# 执行 Git 操作
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push -u origin main
